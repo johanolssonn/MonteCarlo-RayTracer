040a89da5cc358cc475d1942e4c60a0d7d443218
johol920 <johol920@student.liu.se>
2018-10-03 12:37:48 +0200

    Scene hopefully done

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -51,13 +51,12 @@ void Scene::createScene()
 
 
 	const ColorDbl white(1.0, 1.0, 1.0); //Default constructor gives white color
-	const ColorDbl black(0.0, 0.0, 0.0);
 	const ColorDbl red(1.0, 0.0, 0.0);
 	const ColorDbl green(0.0, 1.0, 0.0);
 	const ColorDbl blue(0.0, 0.0, 1.0);
 	const ColorDbl yellow(1.0, 1.0, 0.0);
-	//const ColorDbl black(0.0, 0.0, 0.0);
-	//const ColorDbl black(0.0, 0.0, 0.0);
+	const ColorDbl gray(0.0, 0.0, 0.0);
+	const ColorDbl orange(1.0, 0.5, 0.2);
 
 	//FLOOR
 	const Direction floor_norm( 0.0, 0.0, 1.0 );
@@ -77,6 +76,35 @@ void Scene::createScene()
 	triangle_list.push_back(Triangle(e_ceiling, f_ceiling, origo10_ceiling, white, ceiling_norm));
 	triangle_list.push_back(Triangle(origo_ceiling, origo10_ceiling, f_ceiling, white, ceiling_norm));
 	
-	//WALLS
+	//RIGHT WALL
+	const Direction rWall_norm(0.0 ,1.0, 0.0);
+	triangle_list.push_back(Triangle(f_floor, f_ceiling, e_floor, red, rWall_norm));
+	triangle_list.push_back(Triangle(f_ceiling, e_ceiling, e_floor, red, rWall_norm));
+
+	//LEFT WALL
+	const Direction lWall_norm(0.0, -1.0, 0.0);
+	triangle_list.push_back(Triangle(b_ceiling, b_floor, c_floor, green, lWall_norm));
+	triangle_list.push_back(Triangle(b_ceiling, c_ceiling, c_ceiling, green, lWall_norm));
+
+	//RIGHT BACK WALL
+	const Direction rbWall_norm = CalculateSurfaceNormal(a_floor, a_ceiling, f_floor);
+	triangle_list.push_back(Triangle(a_floor, a_ceiling, f_floor, blue, rbWall_norm));
+	triangle_list.push_back(Triangle(a_ceiling, f_ceiling, f_floor, blue, rbWall_norm));
+
+	//LEFT BACK WALL
+	const Direction lbWall_norm = CalculateSurfaceNormal(b_ceiling, a_ceiling, b_floor);
+	triangle_list.push_back(Triangle(b_ceiling, a_ceiling, b_floor, yellow, lbWall_norm));
+	triangle_list.push_back(Triangle(a_ceiling, a_floor, b_floor, yellow, lbWall_norm));
+
+	//RIGHT FRONT WALL
+	const Direction rfWall_norm = CalculateSurfaceNormal(e_floor, e_ceiling, d_floor);
+	triangle_list.push_back(Triangle(e_floor, e_ceiling, d_floor, gray, rfWall_norm));
+	triangle_list.push_back(Triangle(e_ceiling, d_ceiling, d_floor, gray, rfWall_norm));
+
+	//LEFT FRONT WALL
+	const Direction lfWall_norm = CalculateSurfaceNormal(c_floor, d_floor, c_ceiling);
+	triangle_list.push_back(Triangle(c_floor, d_floor, c_ceiling, orange, lfWall_norm));
+	triangle_list.push_back(Triangle(d_floor, d_ceiling, c_ceiling, orange, lfWall_norm));
+
 
 };

b1fce88d78e69f5a5521a6ad4595945f8bf84c9a
johol920 <johol920@student.liu.se>
2018-10-03 11:55:29 +0200

    Ceiling done, floor variables updated

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -21,12 +21,23 @@ Scene::Scene() {}
 //  
 void Scene::createScene()
 {
-	const Vertex a{ -3.0f, 0.0f, -5.0f };
-	const Vertex b{ -0.0f, 6.0f, -5.0f };
-	const Vertex c{ 10.0f, 6.0f, -5.0f };
-	const Vertex d{ 13.0f, 0.0f, -5.0f };
-	const Vertex e{ 10.0f, -6.0f, -5.0f };
-	const Vertex f{ 0.0f, -6.0f, -5.0f };
+	const Vertex a_floor{ -3.0f, 0.0f, -5.0f };
+	const Vertex b_floor{ -0.0f, 6.0f, -5.0f };
+	const Vertex c_floor{ 10.0f, 6.0f, -5.0f };
+	const Vertex d_floor{ 13.0f, 0.0f, -5.0f };
+	const Vertex e_floor{ 10.0f, -6.0f, -5.0f };
+	const Vertex f_floor{ 0.0f, -6.0f, -5.0f };
+	const Vertex origo_floor{ 0.0f, 0.0f, -5.0f };
+	const Vertex origo10_floor{ 10.0f, 0.0f, -5.0f};
+
+	const Vertex a_ceiling{ -3.0f, 0.0f, 5.0f };
+	const Vertex b_ceiling{ -0.0f, 6.0f, 5.0f };
+	const Vertex c_ceiling{ 10.0f, 6.0f, 5.0f };
+	const Vertex d_ceiling{ 13.0f, 0.0f, 5.0f };
+	const Vertex e_ceiling{ 10.0f, -6.0f, 5.0f };
+	const Vertex f_ceiling{ 0.0f, -6.0f, 5.0f };
+	const Vertex origo_ceiling{ 0.0f, 0.0f, 5.0f };
+	const Vertex origo10_ceiling{ 10.0f, 0.0f, 5.0f};
 
 	const Vertex g{ -3.0f, .0f, 5.0f };
 	const Vertex h{ .0f, .0f, 5.0f };
@@ -37,8 +48,7 @@ void Scene::createScene()
 	const Vertex m{ .0f, .0f, -5.0f };
 	const Vertex n{ -3.0f, .0f, -5.0f };
 
-	const Vertex origo{ 0.0f, 0.0f, 0.0f };
-	const Vertex origo10{ 10.0f, 0.0f, 0.0f};
+
 
 	const ColorDbl white(1.0, 1.0, 1.0); //Default constructor gives white color
 	const ColorDbl black(0.0, 0.0, 0.0);
@@ -51,13 +61,22 @@ void Scene::createScene()
 
 	//FLOOR
 	const Direction floor_norm( 0.0, 0.0, 1.0 );
-	triangle_list.push_back(Triangle(a, f, b, white, floor_norm));
-	triangle_list.push_back(Triangle(b, origo, origo10, white, floor_norm));
-	triangle_list.push_back(Triangle(origo10, c, b, white, floor_norm));
-	triangle_list.push_back(Triangle(e, d, c, white, floor_norm));
-	triangle_list.push_back(Triangle(e, origo10, f, white, floor_norm));
-	triangle_list.push_back(Triangle(origo10, origo, f, white, floor_norm));
+	triangle_list.push_back(Triangle(a_floor, f_floor, b_floor, white, floor_norm));
+	triangle_list.push_back(Triangle(b_floor, origo_floor, origo10_floor, white, floor_norm));
+	triangle_list.push_back(Triangle(origo10_floor, c_floor, b_floor, white, floor_norm));
+	triangle_list.push_back(Triangle(e_floor, d_floor, c_floor, white, floor_norm));
+	triangle_list.push_back(Triangle(e_floor, origo10_floor, f_floor, white, floor_norm));
+	triangle_list.push_back(Triangle(origo10_floor, origo_floor, f_floor, white, floor_norm));
 
+	//CEILING
+	const Direction ceiling_norm(0.0 ,0.0, -1.0);
+	triangle_list.push_back(Triangle(a_ceiling, b_ceiling, f_ceiling, white, ceiling_norm));
+	triangle_list.push_back(Triangle(b, origo10_ceiling, origo_ceiling, white, ceiling_norm));
+	triangle_list.push_back(Triangle(origo10_ceiling, b_ceiling, c_ceiling, white, ceiling_norm));
+	triangle_list.push_back(Triangle(c_ceiling, d_ceiling, e_ceiling, white, ceiling_norm));
+	triangle_list.push_back(Triangle(e_ceiling, f_ceiling, origo10_ceiling, white, ceiling_norm));
+	triangle_list.push_back(Triangle(origo_ceiling, origo10_ceiling, f_ceiling, white, ceiling_norm));
+	
 	//WALLS
 
 };

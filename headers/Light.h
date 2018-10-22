09126f8f4f76346923c29d50357b621844945a13
johol920 <johol920@student.liu.se>
2018-10-22 10:53:02 +0200

    Added light source

---
+++ headers/Light.h
@@ -0,0 +1,19 @@
+#ifndef LIGHT_H
+#define LIGHT_H
+
+#include "Definitions.h"
+#include "ColorDbl.h"
+#include "Triangle.h"
+#include "Scene.h"
+
+#include <vector>
+#include <list>
+
+
+class Light {
+
+    Light();
+};
+
+
+#endif LIGHT_H
---
+++ src/Light.cpp
@@ -0,0 +1,25 @@
+#include "Light.h"
+
+
+Light::Light() {
+
+    // (TOP VIEW)
+
+    // a --------- b
+    // |           |
+    // |           |
+    // c --------- d
+
+
+    const Vertex a{ 4.0, 1.0, 4.99, 1.0 };
+    const Vertex b{ 6.0, 1.0, 4.99, 1.0 };
+    const Vertex c{ 4.0, -1.0, 4.99, 1.0 };
+    const Vertex d{ 6.0, -1.0, 4.99, 1.0 };
+
+    const Direction normal( 0.0, 0.0, -1.0 );
+    const ColorDbl white(1.0, 1.0, 1.0, "LIGHSOURCE");
+
+    //_triangleList.push_back(Triangle(a, d, c, white, normal));
+    //_triangleList.push_back(Triangle(a, b, d, white, normal));
+
+}
